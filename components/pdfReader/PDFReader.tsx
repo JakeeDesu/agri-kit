5df45d5b45d228cf985839314f5e9e0883134fbc
JakeeDesu <hamza.mhindate@gmail.com>
2023-05-01 14:56:03 +0100

    changes

--- components/pdfReader/PDFReader.tsx
+++ components/pdfReader/PDFReader.tsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
-// import PdfViewer from "./pdfViewer"
+import PdfViewer from "./pdfViewer"
 
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 
-const PdfViewer = dynamic(() => import( "./pdfViewer"), {
-  ssr: false,
-});
+// const PdfViewer = dynamic(() => import( "./pdfViewer"), {
+//   ssr: false,
+// });
 
-export default PdfViewer;
+// export default PdfViewer;
 
 export const PDFReader = () => {
-
+    
     return (
-        <div className='w-screen h-screen '>
+        <div className='flex justify-center items-center  w-screen h-screen '>
             <PdfViewer file="" />
         </div>
         )
-}
+    }
+    
--- components/pdfReader/pdfViewer.js
+++ components/pdfReader/pdfViewer.js
@@ -19,13 +19,13 @@ const IndexedPage = React.forwardRef((props, ref) => {
 const PdfViewer = ({ file }) => {
 
   return (
-    <div  className='pt-10 h-full w-full'>
+    <div  className='h-fit w-full md:w-8/12 bg-white'>
       {/* <Document
         file={file}
         onLoadSuccess={onDocumentLoadSuccess}
       > */}
          <HTMLFlipBook
-            // className='w-full h-full'
+            // className=''
             // width={300}
             // height={400 }
             // size="fixed"
@@ -41,7 +41,7 @@ const PdfViewer = ({ file }) => {
           >
             {
             ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15"].map(elem => (
-                    <div key={elem} className="w-full h-full  overflow-hidden">
+                    <div key={elem} className="w-full h-full  overflow-hidden bg-white">
                         <img className="w-full h-full object-contain object-center" src={`/agriEdge/AgriEdge_brochure_V6-${elem}.png`} alt=""/>
                     </div>
             ))
