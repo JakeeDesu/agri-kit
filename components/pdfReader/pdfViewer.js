505036ad37d647178cd97d4d31bbae589bced7f0
JakeeDesu <hamza.mhindate@gmail.com>
2023-05-01 14:34:08 +0100

    changes

--- components/pdfReader/pdfViewer.js
+++ components/pdfReader/pdfViewer.js
@@ -37,6 +37,7 @@ const PdfViewer = ({ file }) => {
             minHeight={400}
             maxHeight={1533}
             maxShadowOpacity={0.5}
+            mobileScrollSupport={true}
           >
             {
             ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15"].map(elem => (
