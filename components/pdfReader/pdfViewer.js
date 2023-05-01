fc29e121c43770843473cad7526b2b828b1f96fb
JakeeDesu <hamza.mhindate@gmail.com>
2023-05-01 14:35:12 +0100

    changes

--- components/pdfReader/pdfViewer.js
+++ components/pdfReader/pdfViewer.js
@@ -42,7 +42,7 @@ const PdfViewer = ({ file }) => {
             {
             ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15"].map(elem => (
                     <div key={elem} className="w-full h-full  overflow-hidden">
-                        <img className="w-full h-full object-contain object-center" src={`/AgriEdge/AgriEdge_brochure_V6-${elem}.png`} alt=""/>
+                        <img className="w-full h-full object-contain object-center" src={`/agriEdge/AgriEdge_brochure_V6-${elem}.png`} alt=""/>
                     </div>
             ))
           }
